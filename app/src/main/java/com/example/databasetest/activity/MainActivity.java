9c1e5b3d08f0f097561feae0f66b092af8994a90
yuanbao97 <316261393@qq.com>
2021-01-25 19:57:40 +0800

    6.4.1 创建数据库

---
+++ app/src/main/java/com/example/databasetest/activity/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.databasetest.activity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.databasetest.R;
+import com.example.databasetest.sqlite.MyDatabaseHelper;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.createBookButton)
+    Button createBookButton;
+
+    private MyDatabaseHelper mDatabaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mDatabaseHelper = new MyDatabaseHelper(this, "BookStore.db", null, 1);
+    }
+
+    @OnClick(R.id.createBookButton)
+    public void onClick() {
+        mDatabaseHelper.getWritableDatabase();
+    }
+}
