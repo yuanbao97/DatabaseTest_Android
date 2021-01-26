af1accf07399cd3adc45039aaf8c7b33a1f65e89
yuanbao97 <316261393@qq.com>
2021-01-26 09:53:28 +0800

    6.4.3 添加数据

--- app/src/main/java/com/example/databasetest/activity/MainActivity.java
+++ app/src/main/java/com/example/databasetest/activity/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.databasetest.activity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,9 +18,11 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @BindView(R.id.createBookButton)
     Button createBookButton;
-
+    @BindView(R.id.insertData)
+    Button insertData;
     private MyDatabaseHelper mDatabaseHelper;
 
     @Override
@@ -27,8 +33,28 @@ public class MainActivity extends AppCompatActivity {
         mDatabaseHelper = new MyDatabaseHelper(this, "BookStore.db", null, 3);
     }
 
-    @OnClick(R.id.createBookButton)
-    public void onClick() {
-        mDatabaseHelper.getWritableDatabase();
+    @OnClick({R.id.createBookButton, R.id.insertData})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.createBookButton:
+                mDatabaseHelper.getWritableDatabase();
+                break;
+            case R.id.insertData:
+                SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values);
+                values.clear();
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values);
+                Toast.makeText(this, "插入成功", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
